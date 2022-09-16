e89909e00a971b4cc84f6259636d7bb1a9254bfa
wimllie <jbpablico27@gmail.com>
2022-09-16 16:28:38 -0500

    Added stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+function init()
+{
+    canvas = document.getElementById("canvas");
+    canvas.width = document.body.clientWidth; //document.width is obsolete
+    canvas.height = document.body.clientHeight; //document.height is obsolete
+    canvasW = canvas.width;
+    canvasH = canvas.height;
+
+    if( canvas.getContext )
+    {
+        setup();
+        setInterval( run , 33 );
+    }
+}
+init();
+console.log(init)
